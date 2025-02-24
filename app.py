f9a9e6e4f9b1fcd0d40e20cf1f42f1d88366624d
Karim <k.abbes@finway.de>
2025-02-24 12:08:25 +0100

    ✨ adds a proxy

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask, request, jsonify
 from youtube_transcript_api import YouTubeTranscriptApi
+from dotenv import load_dotenv
+load_dotenv()  # Loads variables from .env file
+import os
 
 app = Flask(__name__)
 
+proxyUserName = os.getenv('PROXY_USERNAME')
+proxyPassword = os.getenv('PROXY_PASSWORD')
+
 @app.route('/get_transcript', methods=['GET'])
 def get_transcript():
+    proxy = {
+            "https": f"https://{proxyUserName}:{proxyPassword}@gate.smartproxy.com:10001"
+            }
+
     video_id = request.args.get('video_id')
     if not video_id:
         return jsonify({"error": "Missing video_id"}), 400
 
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy)
         transcript_text = " ".join([t['text'] for t in transcript])
         return jsonify({"transcript": transcript_text})
     except Exception as e:
